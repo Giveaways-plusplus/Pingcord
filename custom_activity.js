70e13503b4b2a36513bc6095db6bea4350a8c2a6
Clyde <56137888+Clyde-Beep@users.noreply.github.com>
2020-11-06 09:39:08 -0500

    Update custom_activity.js

--- custom_activity.js
+++ custom_activity.js
@@ -3,15 +3,15 @@
 	//This may not show up immediately - give it about 30s though and it should show up - if not, maybe check the validity of the following
 	const activity = { //https://discord.com/developers/docs/topics/gateway#activity-object
 		type: 0,
-		name: "with OpenDiscord",
-		details: "Visit git.io/JJn7D",
-		state: "or go to github.com/505e06b2/OpenDiscord",
-		application_id: "734267150486732828", //bot -> client_id
+		name: "with Pingcord",
+		details: "Visit git.io/Pingcord",
+		state: "v0.0.1",
+		application_id: "634126243012542484", //bot -> client_id
 		assets: {
-			large_image: "734267782513950740", //bot -> art assets -> upload -> inspect element and get the background code
+			large_image: "ping", //bot -> art assets -> upload -> inspect element and get the background code
 			//large_text: "OpenDiscord",
-			small_image: "734267811953639465",
-			small_text: "Get it on GitHub!"
+			small_image: "null",
+			small_text: "null"
 		}
 	};
 
